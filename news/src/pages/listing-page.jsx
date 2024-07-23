fb2d7f1327da9aea572e01fba66398579e8eff71
john-roufaeil <john.flouis1@gmail.com>
2024-07-23 20:51:40 +0300

    feat: routing

---
+++ news/src/pages/listing-page.jsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+export default function ListingPage() {
+    return (
+        <h1>ListingPage</h1>
+    )
+}
--- news/src/root.component.js
+++ news/src/root.component.js
@@ -1,3 +1,16 @@
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import HomePage from './pages/home-page';
+import ListingPage from './pages/listing-page';
+import DetailsPage from './pages/details-page';
+
 export default function Root(props) {
-  return <section>{props.name} is mounted!</section>;
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/listings" element={<ListingPage />} />
+                <Route path="/details/:id" element={<DetailsPage />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
