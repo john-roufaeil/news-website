0d24ff5a384cdcc13730023b2e33d02e946d0b5d
john-roufaeil <john.flouis1@gmail.com>
2024-07-24 15:05:08 +0300

    feat: add data and listing page

--- news/src/pages/listing-page.jsx
+++ news/src/pages/listing-page.jsx
@@ -1,11 +1,30 @@
 import Header from "../components/Header"
 import Footer from "../components/Footer"
+import List from '@mui/material/List';
+import ListItemText from '@mui/material/ListItemText';
+import ListItemButton from '@mui/material/ListItemButton';
+import data from './data.json';
+import { Link } from 'react-router-dom';
 
 export default function ListingPage() {
     return (
         <>
             <Header />
-            <h1>ListingPage</h1>
+            <section>
+                <h1>News Listing</h1>
+                <List>
+                    {data.map(newsItem => (
+                        <Link key={newsItem.id} to={`/details/${newsItem.id}`}>
+                            <ListItemButton>
+                                <ListItemText
+                                    primary={newsItem.headline}
+                                    secondary={newsItem.abstract}
+                                />
+                            </ListItemButton>
+                        </Link>
+                    ))}
+                </List>
+            </section>
             <Footer />
         </>
     )
